7b84e07d9ab6d3f7ad2ebe6f13f4607d56f2a8fd
citromon95-droid <citromon95@gmail.com>
2026-08-03 12:25:47 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -404,7 +404,7 @@ async function main(): Promise<void> {
 					await server.connect(transport);
 				}
 
-				await transport.handleRequest(req, res);
+				await transport.handleRequest(req, res, req.body);
 				return;
 			}
 
